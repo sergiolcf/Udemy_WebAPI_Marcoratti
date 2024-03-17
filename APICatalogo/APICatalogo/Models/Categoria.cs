efa6cae7542736f8b920fc3329a3fbb6f361a26b
Sergio Ferreira <sergiolc.ferreira@hotmail.com>
2024-03-17 14:47:31 -0300

    feat: Ajuste realizado nos modelos de dominions com Data Annotation

--- APICatalogo/APICatalogo/Models/Categoria.cs
+++ APICatalogo/APICatalogo/Models/Categoria.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.VisualBasic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalogo.Models;
 
+[Table("Categorias")]
 public class Categoria
 {
+    [Key]
     public int Id { get; set; }
+    [Required]
+    [StringLength(80)]
     public string? Nome { get; set; }
+    [Required]
+    [StringLength(300)]
     public string? ImagemUrl { get; set; }
     public ICollection<Produto>? Produtos { get; set; }
 
