84a76b7fa756173ac6ded0e083d7047c28795570
Sergio Ferreira <sergiolc.ferreira@hotmail.com>
2024-03-19 21:13:17 -0300

    feat: Adicionado input inicial no banco via Migrations

---
+++ APICatalogo/APICatalogo/Migrations/20240319232611_PopulaCategorias.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert into categorias(Nome, ImagemUrl) Values('Bebidas', 'bebidas.jpg')");
+            migrationBuilder.Sql("Insert into categorias(Nome, ImagemUrl) Values('Sobremesas', 'sobremesas.jpg')");
+            migrationBuilder.Sql("Insert into categorias(Nome, ImagemUrl) Values('Lanches', 'lanches.jpg')");
+            migrationBuilder.Sql("Insert into categorias(Nome, ImagemUrl) Values('teste', 'teste.jpg')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from categorias");
+        }
+    }
+}
---
+++ APICatalogo/APICatalogo/Migrations/20240319235820_RePopularCategorias.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class RePopularCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert into catalogodb.categorias(Nome, ImagemUrl) Values('Bebidas', 'bebidas.jpg')");
+            migrationBuilder.Sql("Insert into catalogodb.categorias(Nome, ImagemUrl) Values('Lanches', 'lanches.jpg')");
+            migrationBuilder.Sql("Insert into catalogodb.categorias(Nome, ImagemUrl) Values('Sobremesas', 'sobremesas.jpg')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete catalogodb.categorias");
+        }
+    }
+}
