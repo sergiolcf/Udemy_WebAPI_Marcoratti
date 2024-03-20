65587088a889d84f65e32c4990816addb76473f4
Sergio Ferreira <sergiolc.ferreira@hotmail.com>
2024-03-19 21:13:17 -0300

    feat: Adicionado input inicial no banco via Migrations

---
+++ APICatalogo/APICatalogo/Migrations/20240319234525_PopulaProdutos.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert into catalogodb.produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId) Values('Coca-Cola','Refrigerante de Cola 350ml', 5.45,'cocacola.jpg',50,CURRENT_TIMESTAMP,1)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete Produtos");
+        }
+    }
+}
