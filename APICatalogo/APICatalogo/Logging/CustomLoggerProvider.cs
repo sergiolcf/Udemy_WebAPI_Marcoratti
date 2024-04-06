b3bb3e254ebf7d37093da091f0689cdadefc6ae0
Sergio Ferreira <sergiolc.ferreira@hotmail.com>
2024-04-05 22:30:43 -0300

    feat: Adicionado Logger Customizados

---
+++ APICatalogo/APICatalogo/Logging/CustomLoggerProvider.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Concurrent;
+
+namespace APICatalogo.Logging
+{
+    public class CustomLoggerProvider : ILoggerProvider // <- usada para criar instancias de logger personalizadas
+    {
+        readonly CustomerLoggerProviderConfiguration _loggerConfig;
+
+
+        //Dicionario de Loggers <nome da categoria/Componente, Valor da instancia de customerLogger> >
+        readonly ConcurrentDictionary<string, CustomerLogger> _loggers = 
+            new ConcurrentDictionary<string, CustomerLogger>();
+
+
+        //Recebe instancias do CustomerLoogerProviderconfiguration, que define a configuração de todos os loggers criados por este provedor. 
+        public CustomLoggerProvider(CustomerLoggerProviderConfiguration customerLoggerConfig)
+        {
+            _loggerConfig = customerLoggerConfig;
+        }
+        public ILogger CreateLogger(string categoryName) // <- usado para criar logger para uma categoria especifica, retornar um logger existente do dicionario ou criar um novo.
+        {
+            return _loggers.GetOrAdd(categoryName, name => new CustomerLogger(name, _loggerConfig));
+        }
+
+        public void Dispose() //<- liberando recursos quando o provedor for descartado
+        {
+            _loggers.Clear();   
+        }
+    }
+}
---
+++ APICatalogo/APICatalogo/Logging/CustomerLogger.cs
@@ -0,0 +1,55 @@
+﻿
+namespace APICatalogo.Logging
+{
+    public class CustomerLogger : ILogger// <- implementa a interface 'Ilogger', que define os métodos necessarios para registrar as mensagens de logger
+    {
+        readonly string _loggerName; // Receberá o nome da categoria/Componente
+        readonly CustomerLoggerProviderConfiguration _providerConfiguration; //Receberá as configurações do provedor
+
+        public CustomerLogger(string loggerName, CustomerLoggerProviderConfiguration providerConfiguration)
+        {
+            _loggerName = loggerName;
+            _providerConfiguration = providerConfiguration;
+        }
+        public bool IsEnabled(LogLevel logLevel) // <- Verifica se o nível de log desejado esta habilidato na configuração, se não estiver habilitado as msg não serão registradas.
+        {
+            return logLevel == _providerConfiguration.LogLevel;
+        }
+
+
+        //Permite criar um escopo para as mensagens de log, neste exemplo não iremos utilizar
+        public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+        {
+            return null;
+        }
+
+       
+        //Chamado para registrar uma mensagem de log, fará a verificação se o nível de log é permitido, se for, irá usar o delegate 'Func Formatter' e vai escrever a mensagem no arquivo de log através do método 'EscreverTextoNoArquivo
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+            string mensagem = $"{logLevel.ToString()}: {eventId.Id} - {formatter(state, exception)}";
+
+            EscreverTextoNoArquivo(mensagem);
+        }
+
+        //Escreverá a mensagem.
+        private void EscreverTextoNoArquivo(string mensagem)
+        {
+            string caminhoArquivolog = @"D:\Web API ASP .Net 8 Core Essencial\Primeiro Projeto WebAPI\Udemy_WebAPI_Marcoratti\APICatalogo\Log\Macoratti_log.txt";
+
+            using (StreamWriter streamWriter = new StreamWriter(caminhoArquivolog, true))
+            {
+                try
+                {
+                    streamWriter.WriteLine(mensagem);
+                    streamWriter.Close();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+        }
+    }
+}
